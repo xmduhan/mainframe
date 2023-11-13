930c3e8a4189c81f44ac9e2b949f252d58b376af
duhan <duhan@189.cn>
2023-11-13 17:31:44 +0800

    .

--- mainframe/views.py
+++ mainframe/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+from django.template import loader
+
+
+def index(request):
+    """ """
+    context = {}
+    template = loader.get_template("mainframe/main.html")
+    return HttpResponse(template.render(context, request))
+
+
